29d8e37c39473e6eb6245ddc095927263ad68fcf
sebarodriguezm <rodriguezmunoz.seba@gmail.com>
2023-03-28 22:57:27 -0300

    req6

--- app/Http/Controllers/TextoController.php
+++ app/Http/Controllers/TextoController.php
@@ -35,7 +35,7 @@ class TextoController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(TextoRequest $request)
     {
         $texto = new Texto;
         $texto->texto = $request->texto;
